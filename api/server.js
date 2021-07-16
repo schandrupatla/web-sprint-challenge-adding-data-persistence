57807fbfb48edc35a2af775c1585dc82a59704e2
schandrupatla <55516943+schandrupatla@users.noreply.github.com>
2021-07-16 08:10:33 -0700

    added npm packages, server and index file code

--- api/server.js
+++ api/server.js
@@ -1,1 +1,19 @@
 // build your server here and require it from index.js
+const express = require('express');
+const helmet = require('helmet');
+// const router = require('./router');
+
+const server = express();
+
+server.use(helmet());
+server.use(express.json());
+// server.use('/api', router);
+
+server.use((err, req, res, next) => { // eslint-disable-line
+  res.status(500).json({
+    message: err.message,
+    stack: err.stack,
+  });
+});
+
+module.exports = server;
