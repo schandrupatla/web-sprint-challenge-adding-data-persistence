cdba67a0510832b6187e7d5a0734fa5f4ccacf59
schandrupatla <55516943+schandrupatla@users.noreply.github.com>
2021-07-16 12:10:53 -0700

    completed boolean flag for project and middleware validations

--- api/project/router.js
+++ api/project/router.js
@@ -2,27 +2,33 @@
 const express = require('express');
 const db = require('../project/model');
 const router = express.Router();
-
+const {
+  checkProjectPayload
+} = require("./middlewear");
 
 router.get('/', (req,res,next)=>{
     db.getProjects()
         .then(projects=>{
-          if(projects.project_completed !== 1){
-            projects.project_completed === "false"
-        }
-        else{
-          projects.project_completed === "true"
-        }
+            projects.forEach(project=>{
+              if(project.project_completed !== 0){
+                 project.project_completed = "true"
+              }
+              else{
+                project.project_completed = "false"
+              }
+            })
             res.status(200).json(projects);
         })
         .catch(next);
 })
-router.post('/', (req, res, next) => { 
-    db.createProject(req.body)
-      .then(project => {
-        res.status(201).json(project);
-      })
-      .catch(next);
+router.post('/', checkProjectPayload, async (req, res, next) => { 
+  try{
+    const newProject = await db.createProject(req.body)
+        res.status(201).json(newProject);
+    }
+    catch(err){
+      next(err);
+    }
   });
 
 router.use((err, req, res, next) => { // eslint-disable-line
