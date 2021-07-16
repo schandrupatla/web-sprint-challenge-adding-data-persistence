fbab6b7ca374f5670b50b10aca5815ce56bb7b0d
schandrupatla <55516943+schandrupatla@users.noreply.github.com>
2021-07-16 08:12:20 -0700

    added npm packages, server and index file code

--- api/server.js
+++ api/server.js
@@ -8,7 +8,6 @@ const server = express();
 server.use(helmet());
 server.use(express.json());
 // server.use('/api', router);
-
 server.use((err, req, res, next) => { // eslint-disable-line
   res.status(500).json({
     message: err.message,
